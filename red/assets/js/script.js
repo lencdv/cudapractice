1f6beeb2cf700e3d2dc1b87ebace4b23b1f6dbf9
lencdv <36889122+lencdv@users.noreply.github.com>
2018-03-18 13:32:31 +0300

    new

--- red/assets/js/script.js
+++ red/assets/js/script.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
-	$( ".nav__links" ).hide();
 	$( ".hamburger" ).click(function() { 
-		$( ".nav__links" ).toggle( "fast" ); 
+		$( ".nav__links--list").css( "flex-flow", "column nowrap" );
+		$( ".nav__links--list").css( "align-items", "center" );
+		$( ".nav__links" ).toggle( "slow" ); 
 	});
 });
